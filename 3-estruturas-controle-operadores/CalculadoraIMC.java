5d597562f59a43066feeaee26caf8ccfc1f5c2e3
João Paulo <jptick@gmail.com>
2022-06-27 21:01:08 -0300

    Estrutura Condicional

--- 3-estruturas-controle-operadores/CalculadoraIMC.java
+++ 3-estruturas-controle-operadores/CalculadoraIMC.java
@@ -12,29 +12,17 @@ public class CalculadoraIMC {
 
     double imc = peso / (altura * altura);
 
-    entrada.close();
-
     if (imc < 18.5) {
       System.out.println("Abaixo do peso ideal");
-    }
-
-    if (imc >= 18.5 && imc < 25) {
+    } else if (imc < 25) {
       System.out.println("Peso ideal.");
-    }
-
-    if (imc >= 25 && imc < 30) {
+    } else if (imc < 30) {
       System.out.println("Acima do peso.");
-    }
-
-    if (imc >= 30 && imc < 35) {
+    } else if (imc < 35) {
       System.out.println("Obesidade grau I.");
-    }
-
-    if (imc >= 35 && imc < 40) {
+    } else if (imc < 40) {
       System.out.println("Obesidade grau II.");
-    }
-
-    if (imc >= 40) {
+    } else {
       System.out.println("Obesidade grau III.");
     }
 

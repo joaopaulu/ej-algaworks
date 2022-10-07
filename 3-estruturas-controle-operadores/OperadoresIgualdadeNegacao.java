54ccfe3ff98c8e5342accc5d12b5c7a1fa11ffc5
joaopaulu <jptick@gmail.com>
2022-10-07 18:54:35 -0300

    Refazendo o Modulo 3 - Operadores de igualdade e negação

--- 3-estruturas-controle-operadores/OperadoresIgualdadeNegacao.java
+++ 3-estruturas-controle-operadores/OperadoresIgualdadeNegacao.java
@@ -1,12 +1,28 @@
 public class OperadoresIgualdadeNegacao {
-  public static void main(String[] args) {
-    int numero1 = 10;
-    int numero2 = 11;
+    public static void main(String[] args) {
+        //boolean numerosIguais = 10 == 10;
+        //boolean numerosIguais = (4 +6) == (5 * 2);
 
-    boolean numerosIguais = numero1 == numero2;
-    boolean numerosDiferentes = numero1 != numero2;
+        int numero1 = 10;
+        int numero2 = 11;
 
-    System.out.printf("Números iguais: %b%n", numerosIguais);
-    System.out.printf("Números diferentes: %b%n", numerosDiferentes);
-  }
+        //boolean numerosIguais = numero1 == 10;
+        boolean numerosIguais = numero1 == numero2;
+
+        //boolean numerosDiferentes = !numerosIguais;
+        //boolean numerosDiferentes = !(numero1 == numero2);
+        boolean numerosDiferentes = numero1 != numero2;
+
+        System.out.printf("Números iguais: %b%n", numerosIguais);
+
+        System.out.printf("Números diferentes: %b%n", numerosDiferentes);
+
+        String nome1 = "JP";
+        String nome2 = "JP";
+
+        boolean nomesIguais = nome1 == nome2;
+
+        System.out.printf("Nomes iguais: %b%n", nomesIguais);
+    }
 }
+

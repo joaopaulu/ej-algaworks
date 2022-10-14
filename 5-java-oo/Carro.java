b54fc6d88c7566e4b54a505897c2d508617cede8
joaopaulu <jptick@gmail.com>
2022-10-14 20:20:25 -0300

    Atributos de Objetos e UML

--- 5-java-oo/Carro.java
+++ 5-java-oo/Carro.java
@@ -1,6 +1,6 @@
 public class Carro {
-  String fabricante;
-  String modelo;
-  String cor;
-  int anoFabricacao;
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricacao;
 }
--- 5-java-oo/Principal.java
+++ 5-java-oo/Principal.java
@@ -1,8 +1,30 @@
-//Instalar o Plugin Java Visualizer
 public class Principal {
-  public static void main(String[] args) {
-    Carro meuCarro = new Carro();
-    Carro seuCarro = new Carro();
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
 
-  }
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "City";
+
+        Carro seuCarro = new Carro();
+
+        seuCarro.anoFabricacao = 2022;
+        seuCarro.cor = "Braco";
+        seuCarro.fabricante = "Renault";
+        seuCarro.modelo = "Logan";
+
+        System.out.println("Meu Carro");
+        System.out.println("----------");
+        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+
+        System.out.println();
+
+        System.out.println("Seu Carro");
+        System.out.println("----------");
+        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
+
+    }
 }

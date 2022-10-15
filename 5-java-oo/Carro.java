4ebaa90ce5d327f22013a2cd7d15a4ad6250bce5
joaopaulu <jptick@gmail.com>
2022-10-15 17:43:20 -0300

    Implementando metodo

--- 5-java-oo/Carro.java
+++ 5-java-oo/Carro.java
@@ -3,5 +3,20 @@ public class Carro {
     String modelo;
     String cor;
     int anoFabricacao;
+    double precoCompra;
     Pessoa proprietario;
+
+    void calcularValorRevenda() {
+        int tempoDeUsoEmAnos = 2022 - anoFabricacao;
+        int vidaUtilEmAnos = 20;
+
+        double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - tempoDeUsoEmAnos);
+
+        if(valorRevenda < 0){
+            valorRevenda = 0;
+        }
+
+        System.out.printf("Teempo de uso (anos): %d%n", tempoDeUsoEmAnos);
+        System.out.printf("Valor de revenda: %6.2f%n", valorRevenda);
+    }
 }
---
+++ 5-java-oo/TestaCarro.java
@@ -0,0 +1,20 @@
+public class TestaCarro {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "City";
+        meuCarro.precoCompra = 11950;
+
+        Carro seuCarro = new Carro();
+        seuCarro.anoFabricacao = 2010;
+        seuCarro.cor = "Braco";
+        seuCarro.fabricante = "Renault";
+        seuCarro.modelo = "Logan";
+        seuCarro.precoCompra = 50000;
+
+        meuCarro.calcularValorRevenda();
+        seuCarro.calcularValorRevenda();
+    }
+}

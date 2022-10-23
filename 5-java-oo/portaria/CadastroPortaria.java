364bbf05a3f0225072c10968f7ce7dc768981c33
joaopaulu <jptick@gmail.com>
2022-10-23 09:23:19 -0300

    Inferência de tipo de variável local

--- 5-java-oo/portaria/CadastroPortaria.java
+++ 5-java-oo/portaria/CadastroPortaria.java
@@ -9,7 +9,7 @@ public class CadastroPortaria {
     }
 
     void cadastrar(Visitante visitante, final int tempoExpiracaoEmMeses){
-        final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
+        final var tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
 
         System.out.printf("Visitante %s cadastrado para %d dias %n",
                 visitante.nome, tempoExpiracaoEmDias);
--- 5-java-oo/portaria/Principal.java
+++ 5-java-oo/portaria/Principal.java
@@ -2,11 +2,11 @@ package portaria;
 
 public class Principal {
     public static void main(String[] args) {
-        Visitante novoVisitante = new Visitante();
+        var novoVisitante = new Visitante();
         novoVisitante.nome = "JP";
         novoVisitante.idade = 15;
 
-        CadastroPortaria cadastroPortaria = new CadastroPortaria();
+        var cadastroPortaria = new CadastroPortaria();
         cadastroPortaria.cadastrar(novoVisitante, 10);
     }
 }

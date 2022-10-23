1e49f270efe72184ec0d934ee27affc68e7b48da
joaopaulu <jptick@gmail.com>
2022-10-23 09:16:00 -0300

    Sobrecarga de métodos

---
+++ 5-java-oo/portaria/CadastroPortaria.java
@@ -0,0 +1,17 @@
+package portaria;
+
+public class CadastroPortaria {
+
+    static final int TEMPO_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+    void cadastrar(final Visitante visitante){
+        this.cadastrar(visitante, TEMPO_EXPIRACAO_PADRAO_EM_MESES);
+    }
+
+    void cadastrar(Visitante visitante, final int tempoExpiracaoEmMeses){
+        final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
+
+        System.out.printf("Visitante %s cadastrado para %d dias %n",
+                visitante.nome, tempoExpiracaoEmDias);
+    }
+}
--- 5-java-oo/portaria/Principal.java
+++ 5-java-oo/portaria/Principal.java
@@ -1,17 +1,12 @@
 package portaria;
 
-import static portaria.Visitante.IDADE_MINIMA_ACESSO;
-
 public class Principal {
     public static void main(String[] args) {
         Visitante novoVisitante = new Visitante();
         novoVisitante.nome = "JP";
         novoVisitante.idade = 15;
 
-        if(novoVisitante.possuiAcessoPorIdade()){
-            System.out.printf("Acesso não permitido para menores de %d anos", IDADE_MINIMA_ACESSO);
-        }else{
-            System.out.println("Acesso Liberado");
-        }
+        CadastroPortaria cadastroPortaria = new CadastroPortaria();
+        cadastroPortaria.cadastrar(novoVisitante, 10);
     }
 }

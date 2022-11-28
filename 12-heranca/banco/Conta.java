40db5cd85e0a75f60ad9f2952c7d0ab73a89cc06
joaopaulu <jptick@gmail.com>
2022-11-28 10:44:31 -0300

    Criando Classes Etiquetadas

--- 12-heranca/banco/Conta.java
+++ 12-heranca/banco/Conta.java
@@ -2,10 +2,20 @@ package banco;
 
 public class Conta {
 
+    public static final int NORMAL = 0;
+    public static final int INVESTIMENTO = 1;
+    public static final int ESPECIAL = 2;
+
     private Titular titular;
     private int agencia;
     private int numero;
     private double saldo;
+    private int tipo = NORMAL;
+
+    private double valorTotalRendimentos;
+
+    private double tarifaMensal;
+    private double limiteChequeEspecial;
 
     public Titular getTitular() {
         return titular;
@@ -35,12 +45,64 @@ public class Conta {
         return saldo;
     }
 
+    public double getSaldoDisponivel(){
+        return getSaldo() + getLimiteChequeEspecial();
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        if (tipo != NORMAL && tipo != INVESTIMENTO && tipo != ESPECIAL) {
+            throw new IllegalArgumentException("Tipo inválido: " + tipo);
+        }
+        this.tipo = tipo;
+
+        if(this.tipo == ESPECIAL){
+            this.limiteChequeEspecial = 0;
+        }
+    }
+
+    public double getValorTotalRendimentos() {
+        return valorTotalRendimentos;
+    }
+
+    public double getTarifaMensal() {
+        return tarifaMensal;
+    }
+
+    public void setTarifaMensal(double tarifaMensal) {
+        this.tarifaMensal = tarifaMensal;
+    }
+
+    public double getLimiteChequeEspecial() {
+        if(getTipo() != ESPECIAL){
+            throw new RuntimeException("Este tipo de conta não permite limite de cheque especial");
+        }
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public void creditarRendimentos(double percentualJuros) {
+        if (getTipo() == INVESTIMENTO || getTipo() == ESPECIAL) {
+            double valorRendimentos = getSaldo() * percentualJuros / 100;
+            this.valorTotalRendimentos += valorRendimentos;
+            depositar(valorRendimentos);
+        } else {
+            throw new RuntimeException("Não pode creditar rendimentos neste tipo de conta");
+        }
+    }
+
     public void sacar(double valorSaque) {
         if (valorSaque <= 0) {
             throw new IllegalArgumentException("Valor do saque deve ser maior que 0");
         }
 
-        if (saldo < valorSaque) {
+        if (getSaldoDisponivel() < valorSaque) {
             throw new RuntimeException("Saldo insuficiente para saque");
         }
         saldo -= valorSaque;
@@ -59,6 +121,15 @@ public class Conta {
         System.out.printf("Conta: %d%n", getNumero());
         System.out.printf("Titular: %s%n", getTitular().getNome());
         System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponivel());
+    }
+
+    public void debitarTarifaMensal(){
+        if(getTipo() == ESPECIAL){
+            sacar(getTarifaMensal());
+        }else{
+            throw new RuntimeException("Não pode debitar tarifa mensal neste tipo de conta");
+        }
     }
 
 
--- 12-heranca/banco/Principal.java
+++ 12-heranca/banco/Principal.java
@@ -6,7 +6,18 @@ public class Principal {
         conta1.setTitular(new Titular("João Paulo", "00479309183"));
         conta1.setAgencia(0001);
         conta1.setNumero(25165);
+        conta1.setTipo(Conta.ESPECIAL);
+        conta1.setLimiteChequeEspecial(3000);
+        conta1.depositar(13815.84);
+        conta1.creditarRendimentos(6);
+        conta1.setTarifaMensal(90);
 
+        conta1.imprimirDemonstrativo();
+
+        conta1.sacar(15000);
+        conta1.imprimirDemonstrativo();
+
+        conta1.debitarTarifaMensal();
         conta1.imprimirDemonstrativo();
     }
 }

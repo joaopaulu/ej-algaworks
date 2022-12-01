ec67b3ddf0281045a5062f0d4e9a29b5403b2ff7
João Paulo Lima <jptick@gmail.com>
2022-12-01 13:37:28 -0300

    Chamando Método da SuperClasse com super

--- 12-heranca/banco/PrincipalEspecial.java
+++ 12-heranca/banco/PrincipalEspecial.java
@@ -6,11 +6,15 @@ public class PrincipalEspecial {
         conta1.setTitular(new Titular("João Paulo", "00479309183"));
         conta1.setAgencia(0001);
         conta1.setNumero(25165);
-        conta1.depositar(13815.84);
+        conta1.depositar(1000);
+        conta1.setTarifaMensal(90);
 
         conta1.imprimirDemonstrativo();
 
-        conta1.sacar(100);
+        conta1.depositar(100);
+        conta1.imprimirDemonstrativo();
+
+        conta1.sacar(200);
         conta1.imprimirDemonstrativo();
 
         conta1.debitarTarifaMensal();

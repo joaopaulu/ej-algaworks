b3578cde8b9cbb097daee6b58094b4e1312dea09
João Paulo Lima <jptick@gmail.com>
2022-12-14 17:02:14 -0300

    Métodos estáticos em interfaces

--- 14-interfaces/financeira/modelo/ClienteFinanciavel.java
+++ 14-interfaces/financeira/modelo/ClienteFinanciavel.java
@@ -2,22 +2,26 @@ package financeira.modelo;
 
 public interface ClienteFinanciavel {
 
+	double JUROS_BAIXO_RISCO = 1.0;
+	double JUROS_MEDIO_RISCO = 1.5;
+	double JUROS_ALTO_RISCO = 2.0;
+
 	double calcularLimiteAprovado();
 
 	default double calcularJuros(double valorSolitado){
 		if (isFinanciamentoPequenoValor(valorSolitado)) {
-			return 1.0;
+			return JUROS_BAIXO_RISCO;
 		} else if (isFinanciamentoGrandeValor(valorSolitado)) {
-			return 1.5;
+			return JUROS_MEDIO_RISCO;
 		}
-		return 2.0;
+		return JUROS_ALTO_RISCO;
 	}
 
-	private static boolean isFinanciamentoGrandeValor(double valorSolitado) {
+	static boolean isFinanciamentoGrandeValor(double valorSolitado) {
 		return valorSolitado <= 1_000_000;
 	}
 
-	private static boolean isFinanciamentoPequenoValor(double valorSolitado) {
+	static boolean isFinanciamentoPequenoValor(double valorSolitado) {
 		return valorSolitado <= 100_000;
 	}
 }

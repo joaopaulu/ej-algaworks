475674446dfe27645b167d301f5c9375c69f2907
joaopaulu <jptick@gmail.com>
2023-01-07 16:13:14 -0300

    Iniciando o modulo 16 - Execeções

---
+++ 16-excecoes/intro/Principal1.java
@@ -0,0 +1,11 @@
+package intro;
+
+public class Principal1 {
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+
+        int x = 10 / 0;
+
+        System.out.println("Fim");
+    }
+}

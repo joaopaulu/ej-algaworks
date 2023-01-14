73009d8edac23a57bf9627129e264fdc6576d2c0
joaopaulu <jptick@gmail.com>
2023-01-14 14:08:06 -0300

    Variáveis de instância em exceções customizadas

--- 16-excecoes/estoque/exception/ProdutoSemEstoqueException.java
+++ 16-excecoes/estoque/exception/ProdutoSemEstoqueException.java
@@ -2,7 +2,20 @@ package estoque.exception;
 
 public class ProdutoSemEstoqueException extends RuntimeException {
 
-    public ProdutoSemEstoqueException(String message) {
+    private final int estoqueDisponivel;
+    private final int estoqueNecessario;
+
+    public ProdutoSemEstoqueException(String message, int estoqueDisponivel, int estoqueNecessasrio) {
         super(message);
+        this.estoqueDisponivel = estoqueDisponivel;
+        this.estoqueNecessario = estoqueNecessasrio;
+    }
+
+    public int getEstoqueDisponivel() {
+        return estoqueDisponivel;
+    }
+
+    public int getEstoqueNecessario() {
+        return estoqueNecessario;
     }
 }

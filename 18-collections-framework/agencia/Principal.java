ff785f244b1ea287418e89a757d48df74063a1dd
joaopaulu <jptick@gmail.com>
2023-01-28 15:26:05 -0300

    Iniciando o Modulo 18 - Collections Framework

---
+++ 18-collections-framework/agencia/Principal.java
@@ -0,0 +1,16 @@
+package agencia;
+
+public class Principal {
+    public static void main(String[] args) {
+        CadastroHotel cadastro = new CadastroHotel();
+        cadastro.adicionar("Jaguaribe Lodge", "Fortim/CE", 1300);
+        cadastro.adicionar("Vila Selvagem", "Fortim/CE", 1400);
+        cadastro.adicionar("Hotel Fazenda Dona Carolina", "Itatiba/SP", 2200);
+        cadastro.adicionar("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
+        cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
+
+        Hotel hotel = (Hotel) cadastro.obterTodos().get(1);
+        System.out.println(hotel.getNome());
+    }
+
+}

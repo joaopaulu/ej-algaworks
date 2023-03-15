d429ed39eb842a4d2a3c268ab914a38b5c9f68b2
João Paulo Lima <jptick@gmail.com>
2023-03-15 10:52:24 -0300

    Usando conjuntos do tipo TreeSet

---
+++ 18-collections-framework/set/crm/IdadeContatoComparator.java
@@ -0,0 +1,10 @@
+package set.crm;
+
+import java.util.Comparator;
+
+public class IdadeContatoComparator implements Comparator<Contato> {
+	@Override
+	public int compare(Contato o1, Contato o2) {
+		return Integer.compare(o1.getIdade(), o2.getIdade());
+	}
+}
--- 18-collections-framework/set/crm/Principal.java
+++ 18-collections-framework/set/crm/Principal.java
@@ -1,28 +1,24 @@
 package set.crm;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Principal {
     public static void main(String[] args) {
-        Set<Contato> contatos = new HashSet<>();
+        Set<Contato> contatos = new TreeSet<>();
 
+        System.out.println("---");
         contatos.add(new Contato("Maria", "maria@algaworks.com", 40));
         contatos.add(new Contato("Ana", "ana@algaworks.com", 30));
         contatos.add(new Contato("José", "jose@algaworks.com", 25));
         contatos.add(new Contato("Rosa", "rosa@algaworks.com", 50));
         contatos.add(new Contato("João", "joao@algaworks.com", 70));
-        System.out.println("--");
-        contatos.add(new Contato("Josefina", "josefina@algaworks.com", 70));
-        System.out.println("--");
-        contatos.add(new Contato("Josefina", "josefina@algaworks.com", 70));
-        contatos.add(null);
-        System.out.println("--");
+        System.out.println("---");
 
 //        System.out.println(contatos);
 
-        boolean resultado = contatos.contains(
-                new Contato("Alaor", "alaor@algaworks.com", 30));
-        System.out.println(resultado);
+        for (Contato contato : contatos) {
+            System.out.println(contato);
+        }
     }
 }

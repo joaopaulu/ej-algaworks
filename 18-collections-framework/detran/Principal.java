16c66336e76ed1826fac6e2cabeb948d73fe9575
joaopaulu <jptick@gmail.com>
2023-03-20 20:28:13 -0300

    Implementando mapas dos tipos HashMap e Hashtable

--- 18-collections-framework/detran/Principal.java
+++ 18-collections-framework/detran/Principal.java
@@ -1,4 +1,34 @@
 package detran;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Principal {
+    public static void main(String[] args) {
+        Proprietario maria = new Proprietario("Maria", "05577788899");
+        Proprietario ana = new Proprietario("Ana", "33344488811");
+        Proprietario jose = new Proprietario("José", "09988877766");
+        Proprietario joao = new Proprietario("João", "22233344455");
+
+        Carro bmw = new Carro("ALG1A23", "BMW X3");
+        Carro honda = new Carro("WOR9K87", "Honda HR-V");
+        Carro ford = new Carro("JAV5A67", "Ford Ka");
+        Carro porsche = new Carro("EJA0V44", "Porsche 911");
+
+        Map<Carro, Proprietario> proprietarios = new HashMap<>();
+
+        proprietarios.put(bmw, maria);
+        proprietarios.put(honda, ana);
+        proprietarios.put(ford, jose);
+        proprietarios.put(porsche, maria);
+
+        //Substituição de valor para uma chave
+        proprietarios.put(porsche, jose);
+
+        for (var entry : proprietarios.entrySet()) {
+            System.out.printf("%s (%s) = %s%n", entry.getKey().getPlaca(),
+                    entry.getKey().getModelo(), entry.getValue().getNome());
+        }
+
+    }
 }

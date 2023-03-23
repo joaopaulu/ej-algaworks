bd604860aa9f9cb2523893c5f0453461b443be71
joaopaulu <jptick@gmail.com>
2023-03-23 06:41:39 -0300

    Coleções Imutáveis

---
+++ 18-collections-framework/imutaveis/TesteOf.java
@@ -0,0 +1,11 @@
+package imutaveis;
+
+import java.util.List;
+
+public class TesteOf {
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of();
+
+        System.out.println(numeros);
+    }
+}

f1f553e32f610f668220f4c10235a552bd7dd380
joaopaulu <jptick@gmail.com>
2023-03-23 06:48:23 -0300

    Usando a API de List para manipular arrays

---
+++ 18-collections-framework/arrays/Principal.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        Integer[] numeros1 = {1,2};
+        List<Integer> numeros2 = Arrays.asList(numeros1);
+
+        numeros2.set(0, 4);
+
+        System.out.println(Arrays.toString(numeros1));
+        System.out.println(numeros2);
+
+        for (int i = 0; i < numeros2.size(); i++) {
+            System.out.println(numeros2.get(i));
+        }
+    }
+}

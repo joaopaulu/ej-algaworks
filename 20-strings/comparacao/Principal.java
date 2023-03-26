59eede191e310c575802c660adfc494f92e04c46
joaopaulu <jptick@gmail.com>
2023-03-26 09:21:41 -0300

    Iniciando Módulo 20 - Strings

---
+++ 20-strings/comparacao/Principal.java
@@ -0,0 +1,11 @@
+package comparacao;
+
+public class Principal {
+    public static void main(String[] args) {
+        String nome1 = "JP";
+        String nome2 = new String("JP");
+
+        System.out.println(nome1.equals(nome2));
+    }
+
+}

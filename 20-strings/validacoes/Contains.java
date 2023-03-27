41fc2ad939e09423ecff6de82874d1bf9e787314
joaopaulu <jptick@gmail.com>
2023-03-27 19:31:39 -0300

    Validações com Strings

---
+++ 20-strings/validacoes/Contains.java
@@ -0,0 +1,9 @@
+package validacoes;
+
+public class Contains {
+    public static void main(String[] args) {
+        String nome1 = "João da Silva Souza";
+
+        System.out.println(nome1.contains("Silva"));
+    }
+}
---
+++ 20-strings/validacoes/IsBlank.java
@@ -0,0 +1,9 @@
+package validacoes;
+
+public class IsBlank {
+    public static void main(String[] args) {
+        String nome1 = "   ";
+
+        System.out.println(nome1.isBlank());
+    }
+}

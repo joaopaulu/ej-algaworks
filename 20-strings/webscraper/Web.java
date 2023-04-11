6aba721c63b5a785a6d577e7830940c33abf92f3
João Paulo Lima <jptick@gmail.com>
2023-04-11 11:43:28 -0300

    Web Scraping

---
+++ 20-strings/webscraper/Web.java
@@ -0,0 +1,20 @@
+package webscraper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Web {
+	public static void main(String[] args) {
+		String html = "<a href=\"mailto:joao@gmail.com\">\n   joao@gmail.com  </a>\n" +
+				"<a>\n   abc@algaworks.com</a><a>xyz@algaworks.com</a>" +
+				"<strong>maria@algaworks.com</strong>";
+
+		String regex = "<.*?>\\s*(?<email>[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3})\\s*</.*?>";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(html);
+
+		while(matcher.find()){
+			System.out.println(matcher.group("email"));
+		}
+	}
+}

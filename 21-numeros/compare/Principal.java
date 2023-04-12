c156a0f6c15e5d42e9ca71f278242681e188480a
João Paulo Lima <jptick@gmail.com>
2023-04-12 10:55:29 -0300

    Iniciando módulo 21 - Trabalhando com números

---
+++ 21-numeros/compare/Principal.java
@@ -0,0 +1,14 @@
+package compare;
+
+public class Principal {
+	public static void main(String[] args) {
+		int x = 128;
+		int y = 128;
+
+		Integer a = 128;
+		Integer b = 128;
+
+		System.out.println(x == y);
+		System.out.println(a.equals(b));
+	}
+}

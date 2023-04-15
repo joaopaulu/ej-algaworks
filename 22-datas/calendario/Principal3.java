2494829fbc79e851a3fc703ef6fef34abcf93e42
joaopaulu <jptick@gmail.com>
2023-04-15 16:28:04 -0300

    Finalizando módulo 22 - Date Calendar

---
+++ 22-datas/calendario/Principal3.java
@@ -0,0 +1,19 @@
+package calendario;
+
+import java.util.Calendar;
+
+public class Principal3 {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.DAY_OF_MONTH, 20);
+
+//        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+        calendar.add(Calendar.MONTH, 3);
+//        calendar.add(Calendar.MONTH, -3);
+
+        int ultimoDiaDoMes = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        calendar.set(Calendar.DAY_OF_MONTH, ultimoDiaDoMes);
+
+        System.out.println(calendar.getTime());
+    }
+}

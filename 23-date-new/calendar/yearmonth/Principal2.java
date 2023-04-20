4edf6efaa2aa7ea5867aef2a66f1c5fc573a08bc
joaopaulu <jptick@gmail.com>
2023-04-20 11:49:04 -0300

    Representando Ano, Mês e dia

---
+++ 23-date-new/calendar/yearmonth/Principal2.java
@@ -0,0 +1,17 @@
+package calendar.yearmonth;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        YearMonth anoMes = YearMonth.parse("2023-09");
+        System.out.println(anoMes);
+
+        LocalDate dataAniversario = anoMes.atDay(13);
+        System.out.println(dataAniversario);
+
+        LocalDate dataUltimoDiaDoMes = anoMes.atEndOfMonth();
+        System.out.println(dataUltimoDiaDoMes);
+    }
+}

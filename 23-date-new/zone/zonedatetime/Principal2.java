a26a6bcc26e265b62494bdaca6e91283ff09350a
joaopaulu <jptick@gmail.com>
2023-04-22 17:43:58 -0300

    Calculando e convertendo de/para ZonedDateTime

---
+++ 23-date-new/zone/zonedatetime/Principal2.java
@@ -0,0 +1,17 @@
+package zone.zonedatetime;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        LocalDateTime dataLocalDaFesta = LocalDateTime.parse("2023-09-13T20:00:00");
+
+        ZonedDateTime dataDaFestaFusoPadrao = dataLocalDaFesta
+                .atZone(ZoneId.of("America/Los_Angeles"));
+
+        System.out.println(dataLocalDaFesta);
+        System.out.println(dataDaFestaFusoPadrao);
+    }
+}

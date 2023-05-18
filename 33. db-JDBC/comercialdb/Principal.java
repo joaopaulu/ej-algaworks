c41d4719d6d58f52f7c34138396ac72b9817d0fd
João Paulo Lima <jptick@gmail.com>
2023-05-18 15:23:40 -0300

    Executando consultas SQL com Statement

--- 33. db-JDBC/comercialdb/Principal.java
+++ 33. db-JDBC/comercialdb/Principal.java
@@ -1,19 +1,29 @@
 package comercialdb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
 
 public class Principal {
+
 	public static void main(String[] args) {
 		try (Connection conexao = DriverManager
-				.getConnection("jdbc:mysql://localhost:3306/comercial", "root", "")) {
-			System.out.println("Conexão aberta!");
+				.getConnection("jdbc:mysql://localhost:3306/comercial", "root", "");
+			 Statement comando = conexao.createStatement();
+			 ResultSet resultado = comando.executeQuery("select * from venda")) {
+			while (resultado.next()) {
+				Long id = resultado.getLong("id");
+				String nomeCliente = resultado.getString("nome_cliente");
+				BigDecimal valorTotal = resultado.getBigDecimal("valor_total");
+				Date dataPagamento = resultado.getDate("data_pagamento");
 
-			System.out.println(conexao.getClass());
+				System.out.printf("%d - %s - R$%.2f - %s%n",
+						id, nomeCliente, valorTotal, dataPagamento);
+			}
+
 		} catch (SQLException e) {
 			System.out.println("Erro de banco de dados");
 			e.printStackTrace();
 		}
 	}
+
 }

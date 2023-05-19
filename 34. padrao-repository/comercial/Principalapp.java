b175b15f0e75dc97bf0d6efa2b3e14a330a05408
João Paulo Lima <jptick@gmail.com>
2023-05-19 09:14:32 -0300

    Implementando uma consulta no repositório

--- 34. padrao-repository/comercial/Principalapp.java
+++ 34. padrao-repository/comercial/Principalapp.java
@@ -1,6 +1,7 @@
 package comercial;
 
 import comercial.entidade.Venda;
+import comercial.repositorio.VendaRepositorio;
 import comercial.servico.CadastroVendaServico;
 
 import java.math.BigDecimal;
@@ -9,11 +10,16 @@ import java.time.LocalDate;
 public class Principalapp {
 
 	public static void main(String[] args) {
+		var vendaRepositorio = new VendaRepositorio();
 		var cadastroVendaServico = new CadastroVendaServico();
 		Venda vendaCadastrada = cadastroVendaServico.cadastrar("José da Silva",
 				new BigDecimal("12300.87"), LocalDate.parse("2023-04-19"));
 
-		System.out.println(vendaCadastrada);
+		System.out.println("Venda cadastrada: " + vendaCadastrada);
+
+		System.out.println("Listando todas as vendas:");
+		var todasVendas = vendaRepositorio.consultar();
+		todasVendas.forEach(System.out::println);
 	}
 
 }
